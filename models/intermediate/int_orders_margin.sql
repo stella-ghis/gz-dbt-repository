9d4e9fca051cd99a1c7696cf216bc062cbcf513f
stella.ghis@gmail.com <stella.ghis@gmail.com>
2024-10-29 14:55:38 +0000

    encore !!!

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,19 +1,7 @@
-WITH raw_sales AS (
-SELECT *
-FROM {{ ref("stg_raw__sales")}} 
-
-),
-
-raw_products AS (
-    SELECT *
-    FROM {{ ref("stg_raw__product")}} 
-)
 
 SELECT orders_id, date_date, SUM (quantity) AS quantity,
     SUM (quantity * purchase_price) AS purchase_cost,
     SUM (ROUND (revenue - (quantity * purchase_price), 2)) AS margin
-FROM raw_sales 
-LEFT JOIN raw_products 
-USING (products_id)
+FROM  {{ ref("int_sales_margin")}}
 GROUP BY orders_id, date_date
 
--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,4 +1,4 @@
-WITH margin AS (
+WITH orders_margin AS (
 SELECT *
 FROM {{ ref("int_orders_margin")}} 
 
@@ -10,9 +10,10 @@ raw_ship AS (
 )
 
 SELECT orders_id, date_date, SUM (quantity) AS quantity,
-    SUM (purchase_cost) AS purchase_cost,
-    SUM (margin) AS margin
-FROM margin
+    ROUND (SUM (purchase_cost), 2) AS purchase_cost,
+    ROUND (SUM (margin), 2) AS margin,
+    ROUND (SUM (margin + shipping_fee - logcost - ship_cost), 2) AS Operational_margin
+FROM orders_margin
 LEFT JOIN raw_ship
 USING (orders_id)
 GROUP BY orders_id, date_date
